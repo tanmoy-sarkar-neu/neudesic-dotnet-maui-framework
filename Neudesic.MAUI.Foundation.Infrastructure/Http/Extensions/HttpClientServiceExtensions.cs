a32c236628848b093ab0b47c8381bdd4d790e775
Geethu Adhikarveedu <Geethu.Adhikarveedu@neudesic.com>
2021-07-29 15:14:39 +0530

    renamed the file

--- Neudesic.MAUI.Foundation.Infrastructure/Http/Extensions/HttpClientServiceExtensions.cs
+++ Neudesic.MAUI.Foundation.Infrastructure/Http/Extensions/HttpClientServiceExtensions.cs
@@ -4,11 +4,10 @@ using System;
 
 namespace Neudesic.MAUI.Foundation.Infrastructure
 {
-    public static class HttpClientConfigurator 
+    public static class HttpClientServiceExtensions
     {
         public static void RegisterRemoteService<TRefitContract>(this IServiceCollection services, Uri baseUrl, string policyName = "", PolicyConfig policyConfig = null) where TRefitContract : class
-        {
-            
+        {           
             var httpClientBuilder = services.AddRefitClient<TRefitContract>()
             .ConfigureHttpClient(client => client.BaseAddress = baseUrl);
             if (policyConfig != null)
